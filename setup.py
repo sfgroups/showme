d8b1cff291dc29323496ee0340d8c7ffb01b0066
software groups <admin@sfgroups.com>
2023-01-15 15:11:21 -0500

    added new code

--- setup.py
+++ setup.py
@@ -3,24 +3,44 @@ import pathlib
 from setuptools import find_packages, setup
 import setuptools_scm
 
+def myversion():
+    from setuptools_scm.version import SEMVER_MINOR, guess_next_simple_semver, release_branch_semver_version
 
-def get_version(rel_path: str):
-    file_path = pathlib.Path(__file__).parent.absolute() / rel_path
-    for line in file_path.read_text().splitlines():
-        if line.strip().lower().startswith("__version__"):
-            return line.split('=')[1]
-    raise RuntimeError("Unable to find version string.")
+    def my_release_branch_semver_version(version):
+        v = release_branch_semver_version(version)
+        if v == version.format_next_version(guess_next_simple_semver, retain=SEMVER_MINOR):
+            return version.format_next_version(guess_next_simple_semver, fmt="{guessed}", retain=SEMVER_MINOR)
+        return v
 
+    return {
+        'version_scheme': my_release_branch_semver_version,
+        'local_scheme': 'no-local-version',
+    }
+
+
+setup(use_scm_version=myversion)
 
 if __name__ == "__main__":
-    #print(get_version("src/showme/version.py"))
-    try:
-        setup()
-    except:  # noqa
-        print(
-            "\n\nAn error occurred while building the project, "
-            "please ensure you have the most updated version of setuptools, "
-            "setuptools_scm and wheel with:\n"
-            "   pip install -U setuptools setuptools_scm wheel\n\n"
-        )
-        raise
+    print(myversion())
+
+#
+# def get_version(rel_path: str):
+#     file_path = pathlib.Path(__file__).parent.absolute() / rel_path
+#     for line in file_path.read_text().splitlines():
+#         if line.strip().lower().startswith("__version__"):
+#             return line.split('=')[1]
+#     raise RuntimeError("Unable to find version string.")
+#
+#
+# if __name__ == "__main__":
+#     #print(get_version("src/showme/version.py"))
+#     try:
+#         setup()
+#     except:  # noqa
+#         print(
+#             "\n\nAn error occurred while building the project, "
+#             "please ensure you have the most updated version of setuptools, "
+#             "setuptools_scm and wheel with:\n"
+#             "   pip install -U setuptools setuptools_scm wheel\n\n"
+#         )
+#         raise
