fa7e7d34ab5dc7720924ca3d1872981f2b7a7991
software groups <admin@sfgroups.com>
2023-01-22 19:37:36 -0500

    reformatd setup.py

--- setup.py
+++ setup.py
@@ -2,26 +2,28 @@ import pathlib
 
 from setuptools import find_packages, setup
 import setuptools_scm
+setup()
 
-def myversion():
-    from setuptools_scm.version import SEMVER_MINOR, guess_next_simple_semver, release_branch_semver_version
-
-    def my_release_branch_semver_version(version):
-        v = release_branch_semver_version(version)
-        if v == version.format_next_version(guess_next_simple_semver, retain=SEMVER_MINOR):
-            return version.format_next_version(guess_next_simple_semver, fmt="{guessed}", retain=SEMVER_MINOR)
-        return v
-
-    return {
-        'version_scheme': my_release_branch_semver_version,
-        'local_scheme': 'no-local-version',
-    }
-
-
-setup(use_scm_version=myversion)
-
-if __name__ == "__main__":
-    print(myversion())
+#
+# def myversion():
+#     from setuptools_scm.version import SEMVER_MINOR, guess_next_simple_semver, release_branch_semver_version
+#
+#     def my_release_branch_semver_version(version):
+#         v = release_branch_semver_version(version)
+#         if v == version.format_next_version(guess_next_simple_semver, retain=SEMVER_MINOR):
+#             return version.format_next_version(guess_next_simple_semver, fmt="{guessed}", retain=SEMVER_MINOR)
+#         return v
+#
+#     return {
+#         'version_scheme': my_release_branch_semver_version,
+#         'local_scheme': 'no-local-version',
+#     }
+#
+#
+# setup(use_scm_version=myversion)
+#
+# if __name__ == "__main__":
+#     print(myversion())
 
 #
 # def get_version(rel_path: str):
